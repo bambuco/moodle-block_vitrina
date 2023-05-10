6dc2e551dcff22a455e5fcd0b461f72915739fbf
David Herney <davidherney@gmail.com>
2023-05-10 17:34:00 -0500

    Chande default selected categories

--- settings.php
+++ settings.php
@@ -169,7 +169,7 @@ if ($ADMIN->fulltree) {
     $title = get_string('categories', 'block_vitrina');
     $help = get_string('categories_help', 'block_vitrina');
     $displaylist = \core_course_category::make_categories_list('moodle/category:manage');
-    $default = array_keys($displaylist);
+    $default = [];
 
     $setting = new admin_setting_configmultiselect_autocomplete (
         'block_vitrina/categories',
