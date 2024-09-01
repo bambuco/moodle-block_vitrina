c55e41a3e1e245f85bd6453619bc2ced20e47739
David Herney <davidherney@gmail.com>
2024-08-31 20:16:37 -0500

    Stable release

--- version.php
+++ version.php
@@ -24,6 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2023042612.03; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version = 2023042613; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires = 2022112800; // Requires this Moodle version.
 $plugin->component = 'block_vitrina'; // Full name of the plugin (used for diagnostics).
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = 'v4.01.01';
+$plugin->supported = [400, 401, 402, 403];
