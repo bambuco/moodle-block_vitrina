5082da058de4366f46a2f98cc1619d641c5d1e25
David Herney <davidherney@gmail.com>
2024-10-30 23:22:30 -0500

    New release

--- lang/es/block_vitrina.php
+++ lang/es/block_vitrina.php
@@ -151,8 +151,6 @@ $string['textandicon'] = 'Iconos y texto';
 $string['viewall'] = 'Ver todos';
 $string['viewsicons'] = 'Iconos para vistas';
 $string['viewsicons_help'] = 'Elige los íconos que se mostrarán según la vista, normalmente para el menú de pestañas. Uno por línea, con la siguiente estructura: nombrevista=typo/icono. Por ejemplo: greats=i/star. Si no se especifica alguno, se usará el por defecto. Las vistas disponibles son: default, recents, greats, premium.';
-
-// Capabilities.
 $string['vitrina:addinstance'] = 'Adicionar un nuevo bloque de Vitrina de cursos';
 $string['vitrina:myaddinstance'] = 'Adicionar un nuevo bloque de Vitrina de cursos a Dashboard';
 $string['waiting'] = 'Próximamente';
