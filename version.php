69016b5fd09f158b887a544c3f7bbd972fbbbeb0
David Herney <davidherney@gmail.com>
2025-01-14 14:56:13 -0500

    Fixed premium unenrol

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2023042616.01; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version = 2023042616.02; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires = 2022112800; // Requires this Moodle version.
 $plugin->component = 'block_vitrina'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
