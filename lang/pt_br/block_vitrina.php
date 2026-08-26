7379d2340aaa4eebd982068e96c17c10e92ccbc0
Daniele <54783574+DaniFluffyCat@users.noreply.github.com>
2026-08-26 14:40:55 -0400

    Add translation for pt-br

---
+++ lang/pt_br/block_vitrina.php
@@ -0,0 +1,162 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'block_vitrina', language 'pt_br', translated by @DaniFluffyCat
+ *
+ * @package   block_vitrina
+ * @copyright 2020 David Herney @ BambuCo
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['amountcourses'] = 'Qtd. cursos na visão principal';
+$string['amountcourses_help'] = 'Quantidade de cursos a ser exibido na visão principal.';
+$string['catalog'] = 'Catálogo';
+$string['categories'] = 'Categorias';
+$string['categories_help'] = 'Escolha as categorias com os cursos a serem exibidos na visão principal';
+$string['catfilterview'] = 'Exibição do filtro de categoria';
+$string['catfilterview_default'] = 'Padrão';
+$string['catfilterview_directory'] = 'Diretório';
+$string['catfilterview_help'] = 'Escolha a forma que o filtro de categorias será exibido:<br> - Padrão: exibe uma lista simples de categorias.<br> - Árvore: exibe uma visualização em árvore com categorias recuadas.<br> - Diretório: categorias que possuem subcategorias não são usadas como filtro, apenas como título.';
+$string['catfilterview_tree'] = 'Árvore';
+$string['comment_by'] = 'Por {$a}';
+$string['commentslabel'] = '{$a} comentários';
+$string['commentsmanager'] = 'Gerenciador de comentários';
+$string['commentsmanager_help'] = 'Escolha o plugin a ser usado para os comentários do curso';
+$string['commentsmodaltitle'] = 'Comentários do curso';
+$string['commentstitle'] = 'Comentários mais novos';
+$string['commentstitlealternate'] = 'Avaliações do curso';
+$string['completed'] = 'Curso concluído';
+$string['completepercent'] = '{$a}% concluído';
+$string['countstars'] = '{$a} estrelas';
+$string['coursecategory'] = 'Categoria do curso';
+$string['coursedetail'] = 'Detalhes do curso';
+$string['coursedetailtitle'] = 'Curso: {$a->fullname}';
+$string['courselinkcopiedtoclipboard'] = 'Link copiado';
+$string['coverimagetype'] = 'Imagem de capa';
+$string['coverimagetype_default'] = 'Imagem padrão do plugin';
+$string['coverimagetype_generated'] = 'Textura gerada aleatoriamente';
+$string['coverimagetype_help'] = 'Opção para quando um curso não possui imagem.';
+$string['coverimagetype_none'] = 'Nenhuma';
+$string['customgrenroll'] = 'Inscreva-se no grupo selecionado';
+$string['customtitle'] = 'Titulo customizado';
+$string['daystoupcoming'] = 'Qtd. dias para exibir como "Em breve"';
+$string['daystoupcoming_help'] = 'Número de dias para considerar os cursos como ativos para exibição na visualização em blocos.';
+$string['defaultsort'] = 'Todos';
+$string['detailinfo'] = 'Informações detalhadas';
+$string['detailinfo_help'] = 'Informações gerais sobre os detalhes do curso.';
+$string['enroll'] = 'Inscrição';
+$string['enrolled'] = 'Você está atualmente inscrito!';
+$string['enrolledended'] = 'Você está matriculado neste curso, mas sua inscrição está atualmente inativa.';
+$string['enrollrequired'] = 'Você precisa se matricular para fazer o curso!';
+$string['filtercustomfields'] = 'Campos personalizados para filtro';
+$string['filtercustomfields_help'] = 'Selecione os campos personalizados que serão usado para filtrar os cursos no catálogo. Apenas campos de seleção e de caixa de seleção estão disponíveis para filtragem.';
+$string['fulltextsearch'] = 'Busca de texto';
+$string['gotocourse'] = 'Ir para o curso';
+$string['hascourseview'] = 'Você tem acesso a este curso!';
+$string['htmlfooter'] = 'Bloco do rodapé';
+$string['htmlheader'] = 'Bloco de cabeçalho';
+$string['includecontactsinlist'] = 'Incluir contatos na lista de cursos';
+$string['includecontactsinlist_help'] = 'Se habilitado, o bloco também exibirá as informações de contato na lista de cursos.';
+$string['includecustomfieldsinlist'] = 'Incluir campos personalizados na lista de cursos';
+$string['includecustomfieldsinlist_help'] = 'Se habilitado, o bloco também exibirá as informações dos campos personalizados na lista de cursos.';
+$string['includehiddencourses'] = 'Incluir cursos ocultos';
+$string['includehiddencourses_help'] = 'Se habilitado, o bloco também exibirá cursos ocultos (não visíveis) caso o usuário tenha acesso a eles.';
+$string['license-cc-0'] = 'Domínio público';
+$string['licensefield'] = 'Campo de licença';
+$string['licensefield_help'] = 'Define um campo de curso para especificar a licença do curso';
+$string['loadmore'] = 'Carregar mais cursos';
+$string['mediafield'] = 'Área de mídia';
+$string['mediafield_help'] = 'Define um campo para especificar uma URL embedável de vídeo de curso.';
+$string['nocoursesview'] = 'Nenhum curso para exibir aqui.';
+$string['nomorecourses'] = 'Você chegou ao fim.';
+$string['notenrollable'] = 'Curso atualmente indisponível para inscrição!';
+$string['notvisible'] = 'Este curso não está disponível.';
+$string['opendetailstarget'] = 'Abrir os detalhes do curso';
+$string['opendetailstarget_blank'] = 'Em uma nova janela';
+$string['opendetailstarget_help'] = 'Escolha onde deseja abrir a página de detalhes do curso.';
+$string['opendetailstarget_self'] = 'Na mesma janela';
+$string['paymentbutton'] = 'Curso pago';
+$string['paymentinfo'] = 'Métodos disponíveis para adquirir o curso.';
+$string['paymentmethods'] = 'Métodos de pagamento';
+$string['paymentrequired'] = 'Este curso tem um custo.';
+$string['privacy:metadata'] = 'O bloco Showcase não armazena nenhum dado em si.';
+$string['ratingmanager'] = 'Gerente de classificação';
+$string['ratingmanager_help'] = 'Escolha o plugin a ser usado no curso de avaliação.';
+$string['ratings'] = 'Classificações';
+$string['ratingslabel'] = 'classificações {$a}';
+$string['recents'] = 'Recente';
+$string['relatedcourses'] = 'Cursos relacionados';
+$string['relatedlimit'] = 'Qtd. de cursos relacionados';
+$string['relatedlimit_help'] = 'Quantidade de cursos relacionados a serem exibidos na página de detalhes do curso. Se definido como 0, os cursos relacionados não serão exibidos.';
+$string['requireauth'] = 'Você precisa <a href="{$a}">fazer login</a> para se inscrever neste curso.';
+$string['returntocatalog'] = 'Voltar para o catálogo';
+$string['selectcategories'] = 'Escolha uma categoria de curso';
+$string['settingsheaderappearance'] = 'Aparência';
+$string['settingsheaderfields'] = 'Áreas do curso';
+$string['settingsheaderfiltering'] = 'Filtragem';
+$string['settingsheaderpayment'] = 'Opções de pagamento';
+$string['sharecourse'] = 'Compartilhe este curso';
+$string['sharecoursedesc'] = 'Você conhece alguém que possa gostar deste curso?<br /> <strong>Mande o link para ele!</strong>';
+$string['shopmanager'] = 'Gerente da loja';
+$string['shopmanager_help'] = 'Escolha o plugin que deseja usar como loja.';
+$string['showallcomments'] = 'Ver tudo';
+$string['showcustomfields'] = 'Exibir campos personalizados';
+$string['showcustomfields_help'] = 'Exibe os campos personalizados na página de detalhes do curso.';
+$string['showicon'] = 'Apenas ícones';
+$string['showlesscomments'] = 'Ocultar comentários';
+$string['showlongcustomfields'] = 'Exibir campos personalizados longos';
+$string['showlongcustomfields_help'] = 'Exibe os campos personalizados na seção de descrição da página de detalhes do curso.';
+$string['showmore'] = 'Ver mais...';
+$string['showmorecomments'] = 'Mostrar mais comentários';
+$string['showtext'] = 'Apenas texto';
+$string['singleamountcourses'] = 'Qtd. cursos no bloco';
+$string['singleamountcourses_help'] = 'Quantidade de cursos a serem exibidos na visualização de bloco padrão.';
+$string['socialnetworks'] = 'Redes sociais';
+$string['socialnetworks_help'] = 'Uma rede por linha, com a seguinte estrutura:<br>
+icon-name|URL<br>
+A URL pode usar as chaves: {url}, {urlencoded} and {name}.';
+$string['sortalphabetically'] = 'Alfabeticamente';
+$string['sortbydefault'] = 'Ordenação padrão';
+$string['sortbydefault_help'] = 'Escolha a ordem em que os cursos serão exibidos na aba "Todos os cursos".';
+$string['sortbyfinishdate'] = 'Por data de término do curso';
+$string['sortbystartdate'] = 'Por data de início do curso';
+$string['sortdefault'] = 'Seguir ordem padrão dos cursos';
+$string['staticfilters'] = 'Filtros estáticos';
+$string['staticfilters_help'] = 'Selecione filtros específicos que deseja usar no catálogo.';
+$string['summary'] = 'Sumário';
+$string['summary_help'] = 'O conteúdo é exibido no topo da página da lista de cursos e nos detalhes do curso.';
+$string['tabtitle_default'] = 'Todos os cursos';
+$string['tabtitle_greats'] = 'Cursos excelentes';
+$string['tabtitle_recents'] = 'Próximos cursos';
+$string['tabview'] = 'Exibição das abas';
+$string['tabview_help'] = 'Escolha o layout em abas quando mais de uma visualização estiver disponível.';
+$string['templatetype'] = 'Tipo de modelo';
+$string['templatetype_help'] = 'Escolha um modelo para alterar a aparência das páginas de blocos.';
+$string['textandicon'] = 'Texto e ícones';
+$string['tokenenroll'] = 'Inscreva-se com token';
+$string['viewall'] = 'Ver tudo';
+$string['viewsicons'] = 'Ícones das visões';
+$string['viewsicons_help'] = 'Escolha os icones para exibir baseado no tipo de visão, normalmente para o menu de abas. Se nenhum for especificado, o icone padrão será utilizado. As visualizações disponíveis são: <br>
+default, recents, greats, premium<br><br>
+Um por linha, com a seguinte estrutura:<br>
+tipo/icone<br><br>
+Exemplo:<br>
+greats=i/star';
+$string['vitrina:addinstance'] = 'Adicionar um novo bloco de Vitrine';
+$string['vitrina:myaddinstance'] = 'Adicione um novo bloco de Vitrine ao Painel de Controle';
+$string['waiting'] = 'Em breve';
